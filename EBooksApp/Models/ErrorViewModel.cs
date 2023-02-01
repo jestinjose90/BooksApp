063d925a2599cb7c234ed9a4786a8e7cfe516e44
Jestin Jose <fjestinjose@neiu.edu>
2023-02-01 17:23:51 -0600

    Add project files.

---
+++ EBooksApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EBooksApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
