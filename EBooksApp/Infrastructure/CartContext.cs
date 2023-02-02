e6415366c83bd09a774674d21b1d5b63e088babe
Jestin Jose <fjestinjose@neiu.edu>
2023-02-02 14:03:29 -0600

    Add it

---
+++ EBooksApp/Infrastructure/CartContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EBooksApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EBooksApp.Infrastructure
+{
+    public class CartContext : DbContext
+    {
+        public CartContext(DbContextOptions<CartContext> options)
+            : base(options)
+            {
+            }
+
+        public DbSet<Page> Pages { get; set; } 
+      
+
+    }
+}
--- EBooksApp/Startup.cs
+++ EBooksApp/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EBooksApp.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -30,6 +31,8 @@ namespace EBooksApp
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+            services.AddDbContextPool<CartContext>(options => options.UseSqlServer());
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
